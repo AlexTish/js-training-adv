c1f39c2d50b2bafbf4d7680fb8637b525c595efa
Aleksandra Tsishkouskaya <alesandra.arti@gmail.com>
2026-01-24 22:49:09 +0100

    add: dynamic data typing

--- script.js
+++ script.js
@@ -18,3 +18,38 @@ console.log(result);
 const resultStr = String(result);
 const firstTwoDigits = resultStr.slice(0, 2);
 console.log(firstTwoDigits);
+
+// Переменная lang может принимать 2 значения: 'ru' 'en'. Написать условия при котором в зависимости от значения lang будут выводится дни недели на русском или английском языке.
+// Решение через if
+let lang = 'ru';  // можно изменить на 'en'
+if (lang === 'ru') {
+    console.log(['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']);
+} else if (lang === 'en') {
+    console.log(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']);
+} else {
+    console.log('Invalid language code');
+}
+// Решение через switch
+switch (lang) {
+    case 'ru':
+        console.log(['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']);
+        break;
+    case 'en':
+        console.log(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']);
+        break;
+    default:
+        console.log('Invalid language code');
+}
+//Решение через многомерный массив без if и switch
+const days = {
+    'ru': ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+    'en':['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+};
+console.log(days[lang] ||'Invalid language code');
+
+// Решить задачу с помощью нескольких тернарных операторов. У нас есть переменная namePerson. Если значение этой переменной “Артем” то вывести в консоль “директор”, если значение “Александр” то вывести в консоль “преподаватель”, с любым другим значением вывести в консоль “студент”.
+let namePerson = 'Артем' 
+    ? console.log('директор')
+    :namePerson === 'Александр' 
+        ? console.log('преподаватель')
+        : console.log('студент');
